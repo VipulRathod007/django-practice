4f9fdba95b774f0bd176d76a7de7884e6aa301f5
Vipul Rathod <vipuldrathod1458@gmail.com>
2024-10-09 19:20:24 +0530

    Setup auto-browser reload

--- learn_django/learn_django/urls.py
+++ learn_django/learn_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
@@ -23,4 +23,6 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
